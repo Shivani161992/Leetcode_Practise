c999a04b7ab736ec4f5901f46e042ccfee2c5ca3
Shivani Singh <Shivani10@mail.usf.edu>
2020-07-21 16:21:06 -0400

    string compression

--- Array/StringCompression.py
+++ Array/StringCompression.py
@@ -1,19 +1,41 @@
 from typing import List
-chars= ["a","a","b","b","c","c","c"]
+chars= ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
 
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        element = []
+        len_nums=len(chars)
         count = 0
-        for i in chars:
-            hold= chars.count(i) # count number of elements
-            if i not in element:
-                element.append(i)
-                count = count + 1
-                if hold != 1:
-                    for j in str(hold):
-                        count = count + 1
-        return count
+        prev=''
+        index=0
+        while index < len_nums:
+            if prev != chars[index]:
+                prev=chars[index]
+                if prev != "" and count >1:
+                    self.insert_helper(count, index)
+                    count = 1
+                    index = index + 1
+                    len_nums = len(chars)
+                else:
+                    count = 1
+                    index = index + 1
+            elif prev == chars[index]:
+                chars.pop(index)
+                len_nums = len(chars)
+                count= count +1
+
+        self.insert_helper(count, index)
+
+        return len(chars)
+
+    def insert_helper(self, count, index):
+        if count <= 9 and count > 1:
+            chars.insert(index, count)
+        elif count > 9:
+            for i in str(count):
+                chars.insert(index, i)
+                index = index +1
+
+
 
 
 
