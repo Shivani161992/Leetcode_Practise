2e585c96baa183013c31602de62fa7fc5f1bdbb2
Shivani Singh <Shivani10@mail.usf.edu>
2020-08-23 15:32:37 -0400

    Meeting rooms

--- TryThings/MeetingRooms.py
+++ TryThings/MeetingRooms.py
@@ -1,9 +1,21 @@
 from typing import List
-intervals= [[0,30],[5,10],[15,20]]
+intervals= [[0, 30],[5, 10],[15, 20]]
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-        print()
+        if len(intervals) == 0:
+            return True
+        else:
+            intervals.sort(key=lambda x: x[0])
+            end = intervals [0][1]
+            for i in range(1, len(intervals)):
+                new_end= intervals [i][1]
+                if intervals[i][0] < end:
+                    return False
+                else:
+                    end = new_end
+            return True
+
 
 Obj=Solution()
-Obj.canAttendMeetings(intervals)
+print(Obj.canAttendMeetings(intervals))
 
