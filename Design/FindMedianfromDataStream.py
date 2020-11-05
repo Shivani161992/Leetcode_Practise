4fad4affcb8f4f199dcdfc9437d8a43cf373dd46
Shivani Singh <shivani@VPN-157-170.vpn.usf.edu>
2020-11-05 00:01:43 -0500

    find median from data stream

--- Design/FindMedianfromDataStream.py
+++ Design/FindMedianfromDataStream.py
@@ -1,3 +1,9 @@
+
+#this is brute force method
+#In this  method we are adding elements in a array
+#now adding the element in a array is costly. space complexity is O(n) where n is the number of elements
+#time complexity of adding a number is O(1) though
+#time complexity of finding a median is O(nlogn) because we have to sort the elements also
 class MedianFinder:
 
     def __init__(self):
@@ -24,9 +30,39 @@ class MedianFinder:
             return sum
 
 
+#here we will be using the concept of two heaps
+#it will keep track of finding the median
+# what we need to do is keep balancing the min heap and max heap
+#create a max heap and min heap. In max heap all elements will be less than median and in min heap all the elements will be greater than median
+
+import heapq
+class MedianFinder_heap:
+
+    def __init__(self):
+        self.max_lmedian=[]
+        self.min_gmedian=[]
 
 
-obj=MedianFinder()
+
+    def addNum(self, num: int) -> None:
+
+        heapq.heappush(self.max_lmedian, -num)
+        get_max= heapq.heappop(self.max_lmedian)
+        heapq.heappush(self.min_gmedian, -get_max)
+
+        if len(self.max_lmedian) < len(self.min_gmedian):
+            get_min = heapq.heappop(self.min_gmedian)
+            heapq.heappush(self.max_lmedian, -get_min)
+
+    def findMedian(self) -> float:
+        if len(self.max_lmedian) != len(self.min_gmedian):
+            get_max = self.max_lmedian[0]
+            return -get_max
+        elif len(self.max_lmedian) == len(self.min_gmedian):
+            avg= (-self.max_lmedian[0] + self.min_gmedian[0]) /2
+            return avg
+
+obj=MedianFinder_heap()
 obj.addNum(1)
 obj.addNum(2)
 print(obj.findMedian())  #1.5
--- TryThings/CSV_Transpose.py
+++ TryThings/CSV_Transpose.py
@@ -1,9 +1,22 @@
 
-a=[1, 4, -1, 3, 2]
+def change_key(input_data, required_key):
+    for k, v in input_data.items():
+        if isinstance(v, dict):
+            change_key(v, required_key)
+        if k == required_key:
+            value= input_data[k]
+            input_data[k] = '*' * len(value)
+            return
+import json
+with open('/Users/shivani/Downloads/Example_Raw.txt') as json_file:
+    input_data = json.load(json_file)
+    scrub_keys = ['name', 'phone', 'address', 'card_number']
+    for key in scrub_keys:
+        change_key(input_data, key)
 
-count = 1
-value = a[0]
-while (value!=-1):
-    value = a[value]
-    count = count + 1
-print(count)
+
+
+
+
+
+
